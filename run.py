4d682d51c339ef0ff14d62d22e114f70e97217f1
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2022-11-14 11:21:24 +0000

    make to function

--- run.py
+++ run.py
@@ -2,74 +2,33 @@ import random
 
 user_score = 0
 player_score = 0
-return_to_code = True
 
-# Here palyer should select his or her choies
-print("rock...paper...scissor")
 
-while return_to_code:
-    # Here user can inter the loop of playiny this game
-    lap = int(input("Enter the number of rounds you want to play? "))
-    
-    # by using of random in here lap of user will increase one by one
-    for num in range(1, lap + 1):
-
-        # Here player will enter the choice and will compare with computer
-        user = input("Enter your choies ?")
-        number = random.randint(1, 3)
-
-        player = ""
-        if number == 1:
-            player = "rock"
-            print("player choies rock")
-        elif number == 2:
-            player = "paper"
-            print("player choies paper")
-        elif number == 3:
-            player = "scissor"
-            print("player choies scissor")
-
-        # in all game we have three choies and eight comparison.
-        if player == "rock" and user == "paper":
-            print("user win")
-            user_score += 1
-
-        elif player == "rock" and user == "scissor":
-            print("player win")
-            player_score += 1
-
-        elif player == "paper" and user == "rock":
-            print("player win")
-            player_score += 1
-
-        elif player == "paper" and user == "scissor":
-            print("user win")
-            user_score += 1
-
-        elif player == "scissor" and user == "paper":
-            print("player win")
-            player_score += 1
-
-        elif player == "scissor" and user == "rock":
-            print("user win")
-            user_score += 1
-            
-        elif player == "scissor" and user == "siccor":
-            print("equals !!!")
-
-        elif player == "rock" and user == "rock":
-            print("equals !!!")
-            
-        elif player == "paper" and user == "paper":
-            print("equals !!!")
-        else:
-            print("Ooops try again !!!")
-
-    print(f"user {user_score} win ")
-    print(f"player {player_score} win")
-
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() == "y":
-        return_to_code = True
+def select_option():
+    user_option=input("Chooes rock, scissors or paper:  \n")
+    if user_option in ["ROck" , "rock" , 'R' , 'r' ]:
+        user_option="r"
+    elif user_option in ["Paper" , "paper" , 'P' , 'p']:
+        user_option ="p"
+    elif user_option in ["Scissor" , "scissor" , 'S' , 's']:
+        user_option = "s"
     else:
-        return_to_code = False
+        print("Enter a valid value ")
+        select_option()
+        return user_option
+
+
+
+def computer_option():
+    computer_select=random.randint(1, 3)
+    if computer_select == 1 :
+        computer_select="r"
+    elif computer_select == 2:
+        computer_select = "p"
+    else:
+        computer_select = "s"
+        return  computer_select
+
+
+        
+
